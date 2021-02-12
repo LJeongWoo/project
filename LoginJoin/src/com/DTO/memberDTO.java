5ebc1e6925264995eb6cb0cc3a72859f8cd7c617
SM320 <SM320@DESKTOP-2KCGJAV>
2021-02-12 20:34:47 +0900

    DTO DAO 추가

---
+++ LoginJoin/src/com/DTO/memberDTO.java
@@ -0,0 +1,54 @@
+package com.DTO;
+
+public class memberDTO {
+	
+	private String Email;
+	private String Id;
+	private String Pw;
+	private String Nick;
+	private String Address;
+		
+	
+	
+	public memberDTO(String email, String id, String pw, String nick, String address) {
+		super();
+		Email = email;
+		Id = id;
+		Pw = pw;
+		Nick = nick;
+		Address = address;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getId() {
+		return Id;
+	}
+	public void setId(String id) {
+		Id = id;
+	}
+	public String getPw() {
+		return Pw;
+	}
+	public void setPw(String pw) {
+		Pw = pw;
+	}
+	public String getNick() {
+		return Nick;
+	}
+	public void setNick(String nick) {
+		Nick = nick;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	
+	
+	
+}
